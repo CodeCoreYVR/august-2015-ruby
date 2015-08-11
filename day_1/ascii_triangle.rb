0f9c4589014a2a85be16e47f6ce970e744ac87f2
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-08-11 09:48:12 -0700

    add alternative solution to ascii triangle

--- day_1/ascii_triangle.rb
+++ day_1/ascii_triangle.rb
@@ -5,6 +5,7 @@ number = gets.to_i
 counter = 1
 
 until counter > number
+  # print " " * (number - counter), "O " * counter, "\n"
   puts "#{" " * (number - counter)}#{'O ' * counter}"
   counter += 1
 end
